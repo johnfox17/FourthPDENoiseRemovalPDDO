bf820010b073017d7dc844c31b9a0798f5e1a4e4
johnfox17 <johnfox17@gmail.com>
2023-11-11 15:55:36 -0700

    Added function to convert image to grayscale and modified to call fourthOrderPDDODiscretization

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 
 import cv2
-
-
+import numpy as np
+import fourthOrderPDDODiscretization as PDDO
 
 
 
 def main():
     lena = cv2.imread('../data/lena.png')
+    lena = cv2.cvtColor(lena, cv2.COLOR_BGR2GRAY)
     noisyLena = cv2.imread('../data/noisyLena.png')
+    noisyLena = cv2.cvtColor(noisyLena, cv2.COLOR_BGR2GRAY)
+    
+    PDDOMethod = PDDO.fourthOrderPDDODiscretization()
+    PDDOMethod.solve()
+
     cv2.imshow('Lean',lena) 
     cv2.imshow('Noisy Lena',noisyLena)
     cv2.waitKey(0)
+    print(np.shape(noisyLena))
 
 
 
