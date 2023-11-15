7f5b0742b5151c982b7b2e2f5c578f87f9cb8a0d
johnfox17 <johnfox17@gmail.com>
2023-11-14 19:52:26 -0700

    Added BVEC40 and BVEC04

--- src/PDDOConstants.py
+++ src/PDDOConstants.py
@@ -1,7 +1,9 @@
-
+import numpy as np
 #Defining constants for PDDO algorithm
 L1 = 1
 L2 = 1
 NX = 512
 NY = 512
 HORIZON = 5.015 #Because the PDE is 4th order
+BVEC40 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0])
+BVEC04 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0])
