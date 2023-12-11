b52b16ce027ff1cc555ea8bd547aa8f4c2162b69
johnfox17 <johnfox17@gmail.com>
2023-12-11 06:33:37 -0700

    Made PDDO modification specific to paper

--- src/PDDOConstants.py
+++ src/PDDOConstants.py
@@ -4,6 +4,8 @@ L1 = 1
 L2 = 1
 NX = 512
 NY = 512
-HORIZON = 5.015 #Because the PDE is 4th order
-BVEC40 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0])
-BVEC04 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0])
+HORIZON = 3.015
+BVEC20 = np.array([0, 0, 0, 2, 0, 0])
+BVEC02 = np.array([0, 0, 0, 0, 2, 0])
+KERNELDIM = 7
+
