a204faeb1f57f584b256138bedb87faaaf99392e
John Fox <johnfox17@gmail.com>
2023-12-21 16:14:55 -0700

    Renamed secondOrderPDDODiscretization.py to calcPDDOLaplacian.py

--- src/calcPDDOLaplacian.py
+++ src/calcPDDOLaplacian.py
@@ -3,28 +3,18 @@ import numpy as np
 from sklearn.neighbors import KDTree
 from numpy.linalg import solve
 
-class secondOrderPDDODiscretization:
-    def __init__(self):
+class calcPDDOLaplacian:
+    def __init__(self, PDDOKernelMesh):
         self.dx = 1/PDDOConstants.NX
         self.dy = 1/PDDOConstants.NY
-        self.l1 = PDDOConstants.L1
-        self.l2 = PDDOConstants.L2
         self.deltaX = PDDOConstants.HORIZON2*self.dx
         self.deltaY = PDDOConstants.HORIZON2*self.dy
         self.bVec20 = PDDOConstants.BVEC20 
         self.bVec02 = PDDOConstants.BVEC02
         self.horizon = PDDOConstants.HORIZON2
         self.kernelDim = PDDOConstants.KERNELDIM2
+        self.PDDOKernelMesh = PDDOKernelMesh
 
-    def createPDDOKernelMesh(self):
-        indexing = 'xy'
-        xCoords = np.arange(self.dx/2, (self.horizon*2 + 1)*self.dx, self.dx)
-        yCoords = np.arange(self.dy/2, (self.horizon*2 + 1)*self.dy, self.dy)
-
-        xCoords, yCoords = np.meshgrid(xCoords, yCoords, indexing=indexing)
-        xCoords = xCoords.reshape(-1, 1)
-        yCoords = yCoords.reshape(-1, 1)
-        self.PDDOKernelMesh = np.array([xCoords[:,0], yCoords[:,0]]).T
 
     def calculateXis(self):
         midPDDONodeCoords = self.PDDOKernelMesh[int((len(self.PDDOKernelMesh)-1)/2),:]
@@ -57,20 +47,10 @@ class secondOrderPDDODiscretization:
         self.g20 = np.array(g20).reshape((self.kernelDim,self.kernelDim))
         self.g02 = np.array(g02).reshape((self.kernelDim,self.kernelDim))
     
-    def normalizeGPolynomials(self):
-        #self.g20 = np.array(self.g20.flatten()/np.linalg.norm(self.g20.flatten(),1)).reshape((self.kernelDim,self.kernelDim))
-        #self.g02 = np.array(self.g02.flatten()/np.linalg.norm(self.g02.flatten(),1)).reshape((self.kernelDim,self.kernelDim))
-        self.g20 = self.g20
-        self.g02 = self.g02
     def combineGPolynomials(self):
-        self.kernel = -(self.g20 + self.g02)
-
-    def createPDDOKernel(self):
-        self.calculateXis()
-        self.calculateGPolynomials()
-        self.normalizeGPolynomials()
-        self.combineGPolynomials()
+        self.kernel = self.g20 + self.g02
 
     def solve(self):
-        self.createPDDOKernelMesh()
-        self.createPDDOKernel()
+        self.calculateXis()
+        self.calculateGPolynomials()
+        self.combineGPolynomials()
