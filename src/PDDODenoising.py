6782cbeddb195991da3570f6b22cd0b6eedace34
John Fox <johnfox17@gmail.com>
2023-12-22 11:03:18 -0700

    Added function applyBoundaryConditions

--- src/PDDODenoising.py
+++ src/PDDODenoising.py
@@ -5,7 +5,9 @@ from numpy.linalg import solve
 import calcPDDOLaplacian
 
 class PDDODenoising:
-    def __init__(self):
+    def __init__(self, noisyLena):
+        self.noisyLena = noisyLena
+        self.numRows, self.numColumns = np.shape(self.noisyLena)
         self.dx = 1/PDDOConstants.NX
         self.dy = 1/PDDOConstants.NY
         self.horizon = PDDOConstants.HORIZON2
@@ -19,12 +21,33 @@ class PDDODenoising:
         yCoords = yCoords.reshape(-1, 1)
         self.PDDOKernelMesh = np.array([xCoords[:,0], yCoords[:,0]]).T
 
+    def applyBoundaryConditions(self):
+        self.denoisedLena = np.pad(self.noisyLena, 1, mode='constant')
+        # Left BC
+        self.denoisedLena[1:self.numRows+1,0] = self.noisyLena[:,0]
+        #Right BC
+        self.denoisedLena[1:self.numRows+1,self.numColumns+1] = self.noisyLena[:,self.numColumns-1]
+        #Top BC
+        self.denoisedLena[self.numRows+1,1:self.numColumns+1] = self.noisyLena[self.numRows-1,:]
+        #Bottom BC
+        self.denoisedLena[0,1:self.numColumns+1] = self.noisyLena[0,:]
+        #Corners
+        self.denoisedLena[-1,0] = self.noisyLena[-1,0]
+        self.denoisedLena[0,0] = self.noisyLena[0,0]
+        self.denoisedLena[0,-1] = self.noisyLena[0,-1]
+        self.denoisedLena[-1,-1] = self.noisyLena[-1,-1]
+        self.denoisedLena = self.denoisedLena.astype(float)
+
+    #def calcLaplacianOfIntensity(self):
 
     def solve(self):
         self.createPDDOKernelMesh()
         PDDOLaplacian = calcPDDOLaplacian.calcPDDOLaplacian(self.PDDOKernelMesh)
         PDDOLaplacian.solve()
         self.PDDOLaplacianKernel = PDDOLaplacian.kernel
-        np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/PDDOLaplacianKernel.csv', self.PDDOLaplacianKernel, delimiter=",",fmt='%3.3f')
+        self.applyBoundaryConditions()
+        #self.calcLaplacianOfIntensity()
+
+        np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/lenaBC.csv', self.denoisedLena, delimiter=",",fmt='%3.3f')
 
 
