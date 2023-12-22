548b4fadf2e8510ea4748c3efc797c5164be3fc4
John Fox <johnfox17@gmail.com>
2023-12-22 11:55:27 -0700

    Created calcCoefficients function

--- src/PDDODenoising.py
+++ src/PDDODenoising.py
@@ -12,6 +12,9 @@ class PDDODenoising:
         self.dx = 1/PDDOConstants.NX
         self.dy = 1/PDDOConstants.NY
         self.horizon = PDDOConstants.HORIZON2
+        self.deltaX = self.horizon*self.dx
+        self.deltaY = self.horizon*self.dy
+        self.deltaMag = np.sqrt(self.deltaX**2 + self.deltaY**2)
 
     def createPDDOKernelMesh(self):
         indexing = 'xy'
@@ -23,15 +26,25 @@ class PDDODenoising:
         self.PDDOKernelMesh = np.array([xCoords[:,0], yCoords[:,0]]).T
 
     def calcLaplacianOfIntensity(self):
-        self.denoisedLena = signal.convolve2d(self.noisyLena, self.PDDOLaplacianKernel, boundary='symm', mode='same')
+        self.denoisedLena = signal.convolve2d(self.noisyLena, self.PDDOLaplacianKernel, boundary='symm', mode='same')*self.dx*self.dy
 
+    def calcCoefficients(self):
+        self.coefficients = np.zeros((self.numRows, self.numColumns))
+        k = 0.1
+        for iRow in range(self.numRows):
+            for iColumn in range(self.numColumns):
+                #print(self.denoisedLena[iRow,iColumn])
+                self.coefficients[iRow, iColumn] = 1/(1+ np.abs(self.denoisedLena[iRow,iColumn]/k)**2)
+    
     def solve(self):
         self.createPDDOKernelMesh()
         PDDOLaplacian = calcPDDOLaplacian.calcPDDOLaplacian(self.PDDOKernelMesh)
         PDDOLaplacian.solve()
         self.PDDOLaplacianKernel = PDDOLaplacian.kernel
         self.calcLaplacianOfIntensity()
+        self.calcCoefficients()
 
-        np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/laplacianImage.csv', self.denoisedLena, delimiter=",",fmt='%3.3f')
+        #np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/coefficients.csv', self.coefficients, delimiter=",",fmt='%3.3f')
+        np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/laplacian.csv', self.denoisedLena, delimiter=",")
 
 
