2e667645e0c87a03b915091810fe46a87486bde6
johnfox17 <johnfox17@gmail.com>
2024-01-04 20:00:21 -0700

    Cleaned up code and image ouput in main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import fourthOrderPDDODiscretization as PDDO4
 import paperDiscretization
 import PDDODenoising
 import calcMetrics
+import time
 
 def main():
     lena = cv2.imread('../data/lena.png')
@@ -14,31 +15,22 @@ def main():
 
     
 
-    #np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\lenaBefore.csv', noisyLena, delimiter=",")
+    np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\lenaBefore.csv', noisyLena, delimiter=",")
 
+    t = time.time()
     paperMethod = paperDiscretization.paperDiscretization(noisyLena)
     paperMethod.solve()
-    #np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\denoisedDespeckledImages_Paper.csv', paperMethod.denoisedDespeckledImages, delimiter=",")
-    np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/denoisedLena_Paper.csv', paperMethod.denoisedDespeckledImages, delimiter=",")
-
+    print('Time Elapsed Paper Method = ', time.time() - t)
+    np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\denoisedLena_Paper.csv', paperMethod.noisyLena, delimiter=",")
+    #np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/denoisedLena_Paper.csv', paperMethod.denoisedDespeckledImages, delimiter=",")'''
+    t = time.time()
     PDDOMethod = PDDODenoising.PDDODenoising(noisyLena)
     PDDOMethod.solve()
-    #np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\denoisedDespeckledImages_PDDO.csv', PDDOMethod.denoisedDespeckledImages, delimiter=",")
-    np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/denoisedLena_PDDO.csv', PDDOMethod.noisyLena, delimiter=",") 
-    
-    #np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\PDDOKernelMesh4.csv', PDDOMethod4.PDDOKernelMesh, delimiter=",")
-    metrics = calcMetrics.calcMetrics(lena, paperMethod.denoisedDespeckledImages, PDDOMethod.noisyLena)
-    metrics.solve()
-    print(metrics.paperPSNR)
-    print(metrics.pddoPSNR)
-    #cv2.imshow('Lean vs Noisy Lena',np.concatenate((lena, noisyLena), axis=1))
-
-    #cv2.imshow('PDDO Edge Detection', filterIM1.filteredImage)
+    print('Time Elapsed PDDO Method = ', time.time() - t)
+    np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\FourthPDENoiseRemovalPDDO\\data\\denoisedLena_PDDO.csv', PDDOMethod.noisyLena, delimiter=",")
+    #np.savetxt('/home/doctajfox/Documents/Thesis/FourthPDENoiseRemovalPDDO/data/denoisedLena_PDDO.csv', PDDOMethod.noisyLena, delimiter=",") 
     
 
-    #cv2.imshow('Noisy Lena',noisyLena)
-    #cv2.waitKey(0)
-    #print(np.shape(noisyLena))
 
 
 
