166f1766cf693c22e02c350a560745a494a85afa
johnfox17 <johnfox17@gmail.com>
2024-01-04 20:02:52 -0700

    Modified calcMetrics.m to include SSIM metrics

--- tools/calcMetrics.m
+++ tools/calcMetrics.m
@@ -1,10 +1,11 @@
+close all;
+clear all;
 addpath('../data')
 lena = single(imread('lena.png'));
 denoisedLenaPaper = table2array(readtable('denoisedLena_Paper.csv'));
 denoisedLenaPDDO = table2array(readtable('denoisedLena_PDDO.csv'));
 
 %filter paper method for huge values
-denoisedLenaPaper = denoisedLenaPaper(2:513,2:513);
 denoisedLenaPaper(denoisedLenaPaper>255)=255;
 denoisedLenaPaper(denoisedLenaPaper<-255)=-255;
 
@@ -14,14 +15,8 @@ figure; imagesc(denoisedLenaPDDO); colormap gray;
 
 %Calculate PSNR
 paperPSNR = 10.*log10(255^2/(norm(lena(:)-denoisedLenaPaper(:)))^2);
-PDDOPSNR = 10.*log10(255^2/(norm(lena(:)-denoisedLenaPDDO(:)))^2);
+pddoPSNR = 10.*log10(255^2/(norm(lena(:)-denoisedLenaPDDO(:)))^2);
 
 %Calculate SSIM
-lenaMean = mean(lena(:));
-paperMean = mean(denoisedLenaPaper(:));
-pddoMean = mean(denoisedLenaPDDO(:));
-lenaVar = var(lena(:));
-paperVar = var(denoisedLenaPaper(:));
-pddoVar = var(denoisedLenaPDDO(:));
-lenaPaperCovar = xcov(lena(:),denoisedLenaPaper(:));
-paperSSIM = (2*lenaMean*paperMean+ 1e-9);
+[mssim_Paper, ssim_map_Paper] = ssim_index(lena, denoisedLenaPaper);
+[mssim_PDDO ssim_map_PDDO] = ssim_index(lena, denoisedLenaPDDO);
